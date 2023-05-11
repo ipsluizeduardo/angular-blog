d37d54352c3d3f49ff15b8af6d44bed2a8d731c8
ipsluizeduardo <ipsluizeduardo@gmail.com>
2023-05-11 16:59:57 -0400

    Novos arquivos

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+    {
+       "id":"1",
+       "title":"ARQUEIRO VERDE ",
+       "description": "ARQUEIRO VERDE RETORNA",
+       "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwRlsUqrtMH02G1_p41tIp-4xnt8RXOvVdbg&usqp=CAU"
+    }, 
+    {
+        "id":"2",
+        "title":"ARRAVERSE",
+        "description": "HAHAHAHA",
+        "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSROxmBn82JtTtBvSwDpf1E-425ijQYbDSpnA&usqp=CAU"
+     },
+     {
+      "id":"3",
+      "title":"MANUTENÇÃO",
+      "description": "VOLTE DEPOIS",
+      "photoCover":""
+   },
+   {
+      "id":"4", 
+      "title":"MANUTENÇÃO",
+      "description": "VOLTE DEPOIS",
+      "photoCover":""
+   },
+
+]
